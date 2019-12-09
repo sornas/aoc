743dee7f5ae3ae9e4a5e48143938145b11b702e1
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-09 07:31:33 +0100

    Add profiling and timing day 9

--- solutions/py/d09.py
+++ solutions/py/d09.py
@@ -1,13 +1,29 @@
 import intcode
 
-input = open("../input/09", "r").readlines()[0]
+def do(input, code):
+    program = [int(x) for x in input.split(",")]
+    c = intcode.Computer(program)
+    c.input = code
+    output = []
+    while c.memory[c.pointer] != 99:
+        c.step()
+        #print(c.relative_base, c.pointer, c.memory)
+        if c.output is not None:
+            output.append(c.output)
+            c.output = None
+    return output
 
-program = [int(x) for x in input.split(",")]
-c = intcode.Computer(program)
-c.input = 2
-while c.memory[c.pointer] != 99:
-    c.step()
-    #print(c.relative_base, c.pointer, c.memory)
-    if c.output is not None:
-        print(c.output)
-        c.output = None
+def pt1(input):
+    return do(input[0], 1)
+
+def pt2(input):
+    return do(input[0], 2)
+
+if __name__ == "__main__":
+    import cProfile
+
+    input = open("../input/09", "r").readlines()
+    print(pt1(input))
+    print(pt2(input))
+    cProfile.run("pt1(input)")
+    cProfile.run("pt2(input)")
