bb432b66d00deb7d4a259389fe45fba7bac202cc
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-13 09:10:22 +0100

    Day 13 py

--- solutions/py/main.py
+++ solutions/py/main.py
@@ -12,8 +12,9 @@ import d09
 import d10
 import d11
 import d12
+import d13
 
-mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12]
+mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13]
 
 timings = [[0 for _ in range(2)] for _ in range(len(mods))]
 clock_type = time.CLOCK_MONOTONIC
