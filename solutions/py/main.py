3b87c1a0630b3c5ef07b1e7f34be5e5cc29e4192
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-16 08:12:42 +0100

    Cleanup 15 and visualize

--- solutions/py/main.py
+++ solutions/py/main.py
@@ -13,8 +13,10 @@ import d10
 import d11
 import d12
 import d13
+import d14
+import d15
 
-mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13]
+mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13, d14, d15]
 
 timings = [[0 for _ in range(2)] for _ in range(len(mods))]
 clock_type = time.CLOCK_MONOTONIC
