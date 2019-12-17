c03a47188746c5e5aea1600ba4fe5fb656d71d48
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-17 10:32:50 +0100

    Day 17 py
    Also skip d16 when calling from main

--- solutions/py/main.py
+++ solutions/py/main.py
@@ -16,14 +16,19 @@ import d13
 import d14
 import d15
 import d16
+import d17
 
 mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, \
-        d11, d12, d13, d14, d15, d16]
+        d11, d12, d13, d14, d15, d16, d17]
+
+skip = [16]
 
 timings = [[0 for _ in range(2)] for _ in range(len(mods))]
 clock_type = time.CLOCK_MONOTONIC
 
 for mod, day in zip(mods, range(len(mods))):
+    if day+1 in skip:
+        continue
     print("Day", str(day+1).zfill(2))
     t0 = time.clock_gettime_ns(clock_type)
     print("Part", 1, mod.pt1(open("../input/" + str(day+1).zfill(2), "r").readlines()))
@@ -38,7 +43,10 @@ for day in range(len(timings)):
     for part in range(2):
         tot += timings[day][part]
 for day in range(len(timings)):
-    for part in range(2):
-        print("day {0}-{1}: {2:.2f}ms\t({3:.1f}%)".format(str(day+1).zfill(2), part+1, \
-            timings[day][part] / 1000000, 100*timings[day][part] / tot))
+    if day+1 in skip:
+        print("day {0} skipped".format(str(day+1)))
+    else:
+        for part in range(2):
+            print("day {0}-{1}: {2:.2f}ms\t({3:.1f}%)".format(str(day+1).zfill(2), part+1, \
+                timings[day][part] / 1000000, 100*timings[day][part] / tot))
 print("sum", tot / 1000000, "ms")
