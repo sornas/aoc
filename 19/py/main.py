663d3b8ab5b0a20d965df40d71ca8d99e50b8595
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-21 18:13:13 +0100

    Call days 18-21 from main

--- 19/py/main.py
+++ 19/py/main.py
@@ -17,36 +17,23 @@ import d14
 import d15
 import d16
 import d17
+import d18
+import d19
+import d20
+import d21
 
 mods = [d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, \
-        d11, d12, d13, d14, d15, d16, d17]
+        d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, \
+        d21]
 
 skip = []
 
-timings = [[0 for _ in range(2)] for _ in range(len(mods))]
-#clock_type = time.CLOCK_MONOTONIC
-
 for mod, day in zip(mods, range(len(mods))):
-    if day+1 in skip:
+    if day+1 == 18:
+        print("input changed between part 1 and part 2, run it separatly")
+        continue
+    elif day+1 in skip:
         continue
     print("Day", str(day+1).zfill(2))
-    #t0 = time.clock_gettime_ns(clock_type)
     print("Part", 1, mod.pt1(open("../input/" + str(day+1).zfill(2), "r").readlines()))
-    #timings[day][0] = time.clock_gettime_ns(clock_type) - t0
-    #t0 = time.clock_gettime_ns(clock_type)
     print("Part", 2, mod.pt2(open("../input/" + str(day+1).zfill(2), "r").readlines()))
-    #timings[day][1] = time.clock_gettime_ns(clock_type) - t0
-
-#print()
-#tot = 0
-#for day in range(len(timings)):
-#    for part in range(2):
-#        tot += timings[day][part]
-#for day in range(len(timings)):
-#    if day+1 in skip:
-#        print("day {0} skipped".format(str(day+1)))
-#    else:
-#        for part in range(2):
-#            print("day {0}-{1}: {2:.2f}ms\t({3:.1f}%)".format(str(day+1).zfill(2), part+1, \
-#                timings[day][part] / 1000000, 100*timings[day][part] / tot))
-#print("sum", tot / 1000000, "ms")
