49280f2be70142633639618c8f8af0eb807fe429
Gustav Sörnäs <gusso230@student.liu.se>
2019-12-21 18:12:32 +0100

    Separate solutions for day 21

---
+++ 19/py/21-1.js
@@ -0,0 +1,10 @@
+// A or B or C:
+// There is a gap somewhere in the next 3 tiles so we need to jump
+or A T
+and B T
+and C T
+not T J
+// . and D
+// We don't die if we jump
+and D J
+walk
---
+++ 19/py/21-2.js
@@ -0,0 +1,23 @@
+// A or B or C:
+// There is a gap somewhere in the next 3 tiles so we need to jump
+or A T
+and B T
+and C T
+not T J
+// . and D
+// We don't die if we jump
+and D J
+// (postfix)
+// . and (H or EI or EF)
+//   H : We can immediatly jump again
+//   EI: We can walk one step and then jump again
+//   EF: We can walk two steps (and then potentially jump again)
+// We don't have any information about tiles after I so we ignore and hope for
+// the best
+not F T
+not T T
+or I T
+and E T
+or H T
+and T J
+run
