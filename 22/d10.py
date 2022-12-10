cf25184223114562e55ac20635383ca3080ede42
Gustav Sörnäs <gustav@sornas.net>
2022-12-10 06:20:49 +0100

    10-2

--- 22/d10.py
+++ 22/d10.py
@@ -1,32 +1,38 @@
 import sys
 
 def main():
-    strength = 1
-    ans = 0
+    x = 1
     step = 0
+    crt = [[] for _ in range(6)]
+
+    def cycle():
+        nonlocal x, step, crt
+        crtx = step % 40
+        crty = step // 40
+        if x in range(crtx-1, crtx+2):
+            crt[crty].append("#")
+        else:
+            crt[crty].append(".")
     
-    def maybe_inc():
-        nonlocal strength, ans, step
-        if (step + 20) % 40 == 0:
-            print("increasing")
-            ans += step * strength
-
-
     for inst in sys.stdin.readlines():
         if "addx" in inst:
+            cycle()
             step += 1
-            maybe_inc()
+            cycle()
             step += 1
-            maybe_inc()
             to_add = int(inst.strip().split(" ")[1])
-            strength += to_add
-            print(step, to_add, strength)
+            x += to_add
         else:
+            cycle()
             step += 1
-            maybe_inc()
-        if step >= 220:
+        if step >= 240:
             break
-    print(ans)
+    # print(ans)
+
+    for y in range(6):
+        for x in range(40):
+            print(crt[y][x], end="")
+        print()
 
 
 main()
